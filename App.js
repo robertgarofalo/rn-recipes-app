0652deca03be2a7c7856f2be3a666a5c2f0ee3ba
Robert <r.s.garofalo87@gmail.com>
2023-01-18 15:54:57 +1100

    add search screen and back button in header

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
 //Navigation
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 //Components
 import Splash from './src/screens/Splash';
 import Home from './src/screens/Home';
-
+import Search from './src/screens/Search';
+import { useNavigation } from '@react-navigation/native';
 const Stack = createStackNavigator();
+
+const BackButton = () => {
+    const navigation = useNavigation();
+    return (
+  <Pressable onPress={() => navigation.goBack()}>
+    <Image style={styles.back} source={require('./assets/back.png')} />
+  </Pressable>
+)
+    }
 
 export default function App() {
 
   return (
         <NavigationContainer> 
-          <Stack.Navigator>
-            <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false}}/>
-            <Stack.Screen name="Home" component={Home} />
+          <Stack.Navigator screenOptions={{ headerTitleAlign: 'center'}}>
+            <Stack.Screen name="Splash" component={Splash} 
+            options={{ headerShown: false}}/>
+            <Stack.Screen name="Home" component={Home} 
+            options={{ headerLeft: null, gestureEnabled: false}}
+            />
+            <Stack.Screen name="Search" component={Search} 
+            options={{ headerLeft: () => <BackButton />}}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       
@@ -23,7 +39,9 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#000',
-  },
+back: {
+  width: 24,
+  height: 24,
+  margin: 16,
+},
 });
--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import { Button, Text } from 'react-native'
+import { Text } from 'react-native'
 import styles from './styles'
 
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 
-const Home = ({ navigation }) => {
+const Search = () => {
     const insets = useSafeAreaInsets();
 
     return (
             <SafeAreaView style={styles.background}>
-                <Text>HOME</Text>    
-                <Button title="search" onPress={() => navigation.navigate('Search')}>Search</Button>            
+                <Text>Search</Text>                
             </SafeAreaView>
     )
 }
 
-export default React.memo(Home)
+export default React.memo(Search)
